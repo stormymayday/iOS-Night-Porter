a38fe28f25366f77634cce2ca562163fbd850d9c
stormymayday <stormymayday7@gmail.com>
2024-06-04 23:23:09 -0700

    Added changeBackground function

--- Night Porter/ViewController.swift
+++ Night Porter/ViewController.swift
@@ -14,6 +14,23 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeBackground(_ sender: Any) {
+        view.backgroundColor = UIColor.darkGray
+        
+        // get everything contained in the top level view
+        let everything = view.subviews
+        
+        for eachView in everything {
+            // is it a label?
+            if eachView is UILabel {
+                // downcast as UILabel
+                let currentLabel = eachView as! UILabel
+                
+                currentLabel.textColor = UIColor.lightGray
+            }
+            // eachView.backgroundColor = UIColor.green
+        }
+    }
+    
 }
 
